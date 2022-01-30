cf5c4a45d3269d9aea25a1d894a869f5bb6d6fad
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-01-30 13:45:06 +0300

    Time: 262 ms (54.13%), Space: 25.7 MB (13.79%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,9 +4,14 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         5 6 7 4 1 2 3,     k=3
         """
+        #O(1) space
         k = k%len(nums)
-        k = len(nums)-k
+        if k>0:
+            nums[:-k], nums[-k:] = nums[-k:], nums[:-k]
+        return
+        #O(n) space
         new = []
+        k = len(nums)-k
         while k<len(nums):
             new.append(nums[k])
             k+=1
