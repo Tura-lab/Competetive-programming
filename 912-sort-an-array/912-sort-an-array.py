6a53c3872adc383a99a31a3ee09db81f6bd0d098
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-01-31 12:14:11 +0300

    Time: 1142 ms (25.61%), Space: 22.7 MB (25.00%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, left, right):
+        new = []
+        i=j=0
+        while  i<len(left) and j<len(right):
+            if left[i]>right[j]:
+                new.append(right[j])
+                j+=1
+            else:
+                new.append(left[i])
+                i+=1
+        while i<len(left):
+            new.append(left[i])
+            i+=1
+        while j<len(right):
+            new.append(right[j])
+            j+=1
+        return new
+            
+    def sortArray(self, nums):
+        if len(nums)<=1:
+            return nums
+        mid   = (len(nums))//2
+        left  = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+        return self.merge(left, right)
