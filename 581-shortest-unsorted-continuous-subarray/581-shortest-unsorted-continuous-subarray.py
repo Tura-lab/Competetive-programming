de117230d6f2dc5428d40b04bf6f00dd39c89d9a
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-02-11 10:34:05 +0300

    Time: 272 ms (51.54%), Space: 15.2 MB (85.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -1,23 +1,37 @@
 class Solution:
+    '''
+    2,6,4,8,10,9,15
+        i
+               j
+    2,4,
+    15,10,8,6,2 
+    s = 1
+    '''
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        s = sorted(nums)
-        i=0
-        j=len(nums)-1
-        
-        start=end=None
-        
+        i=0        
+        start = None
+        stack = []
         while i<len(nums):
-            if nums[i] != s[i]:
-                start = i
-                break
+            while stack and stack[-1]>nums[i]:
+                stack.pop()
+                start = len(stack) if start==None else min(len(stack), start)
+            
+            stack.append(nums[i])
             i+=1
-            
-        if start==None:
-            return 0
         
-        while j>-1: 
-            if nums[j] != s[j]:
-                return j-start+1
+        i = len(nums)-1
+        end = None
+        stack = []
+        while i>-1:
+            while stack and stack[-1]<nums[i]:
+                stack.pop()
+                end = len(stack) if end==None else min(len(stack), end)
             
-            j-=1
-            
+            stack.append(nums[i])
+            i-=1
+        
+        if end==None or start==None:
+            return 0
+        end = len(nums)-end-1
+        
+        return end-start+1
