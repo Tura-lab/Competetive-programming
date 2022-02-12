3826f8103b73747e8de324299cb6f00df0dd61ad
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-02-13 01:28:46 +0300

    Time: 106 ms (10.20%), Space: 14.5 MB (51.82%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        if not len(s)==len(t):
+            return False
+        
+        goal = {}
+        for i in range(len(s)):
+            val = s[i]
+            if val in goal:
+                goal[val] +=1
+            else:
+                goal[val] =1
+        
+        d = {}
+        for i in range(len(t)):
+            val = t[i]
+            if val in d:
+                d[val]+=1
+            else:
+                d[val]=1
+        return goal == d
