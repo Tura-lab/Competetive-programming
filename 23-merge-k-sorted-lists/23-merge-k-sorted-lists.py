16ca1504eb51ccb8bc31e0c493b13f32bf841f1f
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-02-22 16:53:16 +0300

    Time: 114 ms (79.53%), Space: 18.4 MB (19.36%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -6,58 +6,28 @@
 # class Linked_List:
 #     def __init__(self):
 #         self.head = None
+import heapq
 
 class Solution:
-    def merge(self,l1,l2):
-        new = ListNode()
-        h=new
-        while l1 and l2:
-            if l1.val> l2.val:
-                new.next = l2
-                l2 = l2.next
-            else:
-                new.next = l1
-                l1 = l1.next
-            new = new.next
-        new.next = l1 or l2
-        return h.next
-    def merger(self, lists, start, end):
-        if start==end:
-            return lists[start]
-        mid = (start+end)//2
-        left = self.merger(lists, start,mid)
-        right = self.merger(lists, mid+1, end)
-        return self.merge(left,right)
-    
+
     def mergeKLists(self, lists):
-        if lists==[]:
-            return None
-        return self.merger(lists, 0, len(lists)-1)
+        dummy = ListNode()
+        new = dummy
+        
+        heap = []
+        heapq.heapify(heap)
+        
+        for l in lists:
+            while l:
+                heapq.heappush(heap, l.val)
+                l = l.next
+                
+        while heap:
+            new.next = ListNode(heapq.heappop(heap))
+            new = new.next
+        
+        
+        
+        return dummy.next
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        # ls = []
-        # for l in lists:
-        #     temp = l
-        #     while temp:
-        #         ls.append(temp.val)
-        #         temp = temp.next
-        # if ls == []:
-        #     return None
-        # final_LL = Linked_List()
-        # ls = sorted(ls)
-        # final_LL.head = ListNode(ls[0])
-        # current = final_LL.head
-        # for i in ls[1:]:
-        #     current.next = ListNode(i)
-        #     current = current.next
-        # return final_LL.head
         
