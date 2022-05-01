f7d3ad4eea113b80c218583e3472e592979a84c9
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-01 11:10:22 +0300

    Time: 146 ms (5.00%), Space: 14.4 MB (5.77%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,40 +1,88 @@
 from collections import Counter
 import heapq
 
+class TrieNode:
+    def __init__(self, val=None):
+        self.val = val 
+        self.children = [None]*26
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for i in range(len(word)):
+            c = word[i]
+            if node.children[ord(c)-ord('a')] == None:
+                n = TrieNode(val=c)
+                node.children[ord(c)-ord('a')] = n
+            node = node.children[ord(c)-ord('a')]
+        node.isEnd=True        
+        
+
 class Solution:
-    def topKFrequent(self, nums: List[str], k: int) -> List[str]:
-        counts = Counter(nums)
-        mapper = {}
-        for num, count in counts.items():
-            if count in mapper:
-                mapper[count].append(num)
-            else:
-                mapper[count] = [num]
-                
-        for count, nums in mapper.items():
-            nums.sort()
-        minHeap = []
-        heapq.heapify(minHeap)
-
-        for count, nums in mapper.items():
-            for i in range(len(nums)):
-                if len(minHeap) == k:
-                    if minHeap[0] < counts[nums[i]]:
-                        heapq.heappop(minHeap)
-                        heapq.heappush(minHeap, count)
-
-                else:
-                    heapq.heappush(minHeap, count)
-
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        counter = Counter(words)
+        bucket = []
+        for _ in range(len(words)):
+            bucket.append(Trie())
+        for word in words:
+            trie = bucket[counter[word]]
+            trie.insert(word)
+        
         ans = []
-        minHeap = list(set(minHeap))
-        minHeap.sort(reverse=True)
-        for i in range(len(minHeap)):
+        def printer(node, yet):
             if len(ans)==k:
-                break
-            for i in mapper[minHeap[i]]:
-                if len(ans)==k:
-                    break
-                ans.append(i)
-
+                return
+            if node.isEnd:
+                ans.append(yet)
+            if node.children == []:
+                return
+            for n in node.children:
+                if n:
+                    printer(n, yet+n.val)
+            
+        i = len(bucket)-1
+        while len(ans)<k and i>-1:
+            trie = bucket[i]
+            printer(trie.root, '')
+            i-=1
+        
         return ans
+#         counts = Counter(nums)
+#         mapper = {}
+#         for num, count in counts.items():
+#             if count in mapper:
+#                 mapper[count].append(num)
+#             else:
+#                 mapper[count] = [num]
+                
+#         for count, nums in mapper.items():
+#             nums.sort()
+#         minHeap = []
+#         heapq.heapify(minHeap)
+
+#         for count, nums in mapper.items():
+#             for i in range(len(nums)):
+#                 if len(minHeap) == k:
+#                     if minHeap[0] < counts[nums[i]]:
+#                         heapq.heappop(minHeap)
+#                         heapq.heappush(minHeap, count)
+
+#                 else:
+#                     heapq.heappush(minHeap, count)
+
+#         ans = []
+#         minHeap = list(set(minHeap))
+#         minHeap.sort(reverse=True)
+#         for i in range(len(minHeap)):
+#             if len(ans)==k:
+#                 break
+#             for i in mapper[minHeap[i]]:
+#                 if len(ans)==k:
+#                     break
+#                 ans.append(i)
+
+#         return ans
