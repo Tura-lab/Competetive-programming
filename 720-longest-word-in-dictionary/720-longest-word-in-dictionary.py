a7c6a0dc1d8d7baecbecddf61474dedd8511d478
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-03 16:13:22 +0300

    Time: 507 ms (5.35%), Space: 15.7 MB (7.20%) - LeetHub

---
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self, val=None):
+        self.val = val 
+        self.children = [None]*26
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for i in range(len(word)):
+            c = word[i]
+            if node.children[ord(c)-ord('a')] == None:
+                n = TrieNode(val=c)
+                node.children[ord(c)-ord('a')] = n
+            node = node.children[ord(c)-ord('a')]
+        node.isEnd=True
+
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        
+        def dfs(node):
+            longest = ''
+            for n in node.children:
+                if n and n.isEnd:
+                    ans = n.val+dfs(n)
+                    if len(ans) > len(longest):
+                        longest = ans         
+            return longest
+        
+        return dfs(trie.root)
