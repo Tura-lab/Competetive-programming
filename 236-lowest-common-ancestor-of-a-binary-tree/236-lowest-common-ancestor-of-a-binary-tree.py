309af9c7ac278dc8299bec16ab4da5edf534fc1e
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-09 09:58:37 +0300

    Time: 92 ms (55.71%), Space: 26 MB (86.38%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,25 +7,42 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        p_ancesters = []
-        q_ancesters = []
-        roots = []
-        
-        def helper(root, arr):
-            if not root:
-                return
-            if root == p:
-                roots.append (arr + [p])
-            if root == q:
-                roots.append(arr + [q])
+        def dfs(root):
+            if not root or root==p or root==q:
+                return root
             
-            helper(root.left, arr+[root])
-            helper(root.right, arr+[root])
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left and right:
+                return root
+            elif left:
+                return left
+            else:
+                return right
         
-        helper(root, [])
         
-        set1 =set(roots[0])
-        for i in range(len(roots[1])-1,-1,-1):
-            node = roots[1][i]
-            if node in set1:
-                return node
+        return dfs(root)
+        
+        
+#         BAADDD
+#         roots = []
+        
+#         def helper(root, arr):
+#             if not root:
+#                 return
+#             if root == p:
+#                 roots.append (arr + [p])
+#             if root == q:
+#                 roots.append(arr + [q])
+            
+#             helper(root.left, arr+[root])
+#             helper(root.right, arr+[root])
+        
+#         helper(root, [])
+        
+#         set1 =set(roots[0])
+#         for i in range(len(roots[1])-1,-1,-1):
+#             node = roots[1][i]
+#             if node in set1:
+#                 return node
