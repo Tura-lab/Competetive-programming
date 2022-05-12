a7381491df9b13bb97fa3b2c6034d8276cc4ba4c
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-12 17:00:14 +0300

    Time: 106 ms (19.63%), Space: 14 MB (47.73%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,15 +1,28 @@
 class Solution:
+    '''
+    dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+    '''
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [-1]*len(cost)
-        def dfs(i):
-            if i>=len(cost):
-                return 0
-            if dp[i] != -1:
-                return dp[i]
+        dp = [-1 for _ in range(len(cost))]
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+            
+        # print(dp)
+        return min(dp[-1], dp[-2])
+        
+#         dp = [-1]*len(cost)
+#         def dfs(i):
+#             if i>=len(cost):
+#                 return 0
+#             if dp[i] != -1:
+#                 return dp[i]
             
             
-            dp[i] = cost[i] + min(dfs(i+1), dfs(i+2))
-            return dp[i]
+#             dp[i] = cost[i] + min(dfs(i+1), dfs(i+2))
+#             return dp[i]
         
         
-        return min(dfs(0), dfs(1))
+#         return min(dfs(0), dfs(1))
