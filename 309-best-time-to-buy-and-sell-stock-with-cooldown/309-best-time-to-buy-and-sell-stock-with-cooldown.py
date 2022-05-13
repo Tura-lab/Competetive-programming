700907194fa52799a5785887a79a153893a5fc78
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-13 15:26:09 +0300

    Time: 117 ms (9.43%), Space: 16.8 MB (34.74%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,19 +7,19 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # print(len(prices))
         
-        dp = [[-1 for i in range(len(prices))] for _ in range(len(prices))]
+        dp = [-1 for _ in range(len(prices))]
         def dfs(l,r):
             
             if r>=len(prices):
                 return 0
-            if dp[l][r] !=-1:
-                return dp[l][r]
+            if dp[l] !=-1:
+                return dp[l]
             if prices[l] >= prices[r]:
                 return dfs(r,r+1)
             smash = prices[r]-prices[l] + dfs(r+2, r+3)
             passs = dfs(l,r+1)
             
-            dp[l][r] = max(smash, passs)
-            return dp[l][r]
+            dp[l] = max(smash, passs)
+            return dp[l]
                     
         return dfs(0,1)
