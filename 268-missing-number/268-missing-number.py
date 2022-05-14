1c80277e65d8ae049d83017fdbb5090352917d57
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-14 21:20:13 +0300

    Time: 175 ms (58.40%), Space: 15.2 MB (78.11%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,15 +1,10 @@
 class Solution:
-    '''
-    3,0,1
-    '''
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            while -1 < nums[i] < len(nums) and nums[i] != nums[nums[i]]:
-                val = nums[nums[i]]
-                nums[nums[i]] = nums[i]
-                nums[i] = val
+        ans = 0
+        for i in range(len(nums)+1):
+            ans ^= i
+            
+        for num in nums:
+            ans ^= num
         
-        for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        return i+1
+        return ans
