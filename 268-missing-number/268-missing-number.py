bca99208aa65c4229fa72f9e0c56f599efd6455b
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-14 21:22:55 +0300

    Time: 147 ms (78.00%), Space: 15.2 MB (78.11%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(len(nums)+1):
-            ans ^= i
-            
-        for num in nums:
-            ans ^= num
-        
+        ans = len(nums)
+        for i in range(len(nums)):
+            ans += (i-nums[i])
         return ans
+            
+        
+#         ## xor
+#         ans = 0
+#         for i in range(len(nums)+1):
+#             ans ^= i
+            
+#         for num in nums:
+#             ans ^= num
+
+#         return ans
