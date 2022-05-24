52aafc51f54036bee6a3717522dbd66c9383fefc
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-24 10:22:49 +0300

    Time: 44 ms (52.67%), Space: 13.9 MB (79.76%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,25 +1,67 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == '0':
-            return 0
-        for i in range(len(s)-1):
-            if int(s[i])>2 and int(s[i+1])== 0:
-                return 0
-            if s[i] == s[i+1] == '0':
-                return 0
+        dp = [0]*(len(s)+1)
+        dp[0]=1
         
-        @cache
-        def dfs(i):
-            if i>=len(s):
-                return 1
-            if s[i] == '0':
-                return 0
-            tot = 0
+        first = 0
+        second = 1
+        cur = 0
+        
+        for i in range(1, len(s)+1):
+            if s[i-1]!='0':
+                cur += second
+                
+            if i!=1 and s[i-2]!='0' and int(s[i-2]+s[i-1]) < 27:
+                cur += first
+                
+            first =second
+            second =cur
+            cur = 0
             
-            tot += dfs(i+1)
-            if int(s[i])<3 and i<len(s)-1 and (i+1<len(s) and int(s[i]+s[i+1])<27):
-                tot += dfs(i+2)
+        return second
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if s[0] == '0':
+#             return 0
+#         for i in range(len(s)-1):
+#             if int(s[i])>2 and int(s[i+1])== 0:
+#                 return 0
+#             if s[i] == s[i+1] == '0':
+#                 return 0
+        
+#         @cache
+#         def dfs(i):
+#             if i>=len(s):
+#                 return 1
+#             if s[i] == '0':
+#                 return 0
+#             tot = 0
             
-            return tot
+#             tot += dfs(i+1)
+#             if int(s[i])<3 and i<len(s)-1 and (i+1<len(s) and int(s[i]+s[i+1])<27):
+#                 tot += dfs(i+2)
             
-        return dfs(0)
+#             return tot
+            
+#         return dfs(0)
