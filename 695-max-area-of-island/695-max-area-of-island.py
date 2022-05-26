1c62910204b7ca7e512ca45ec40d2b6b14cac07a
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-27 00:44:36 +0300

    Time: 169 ms (62.92%), Space: 17.9 MB (9.23%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,21 +1,26 @@
+class UnionFind:
+    def __init__(self, n):
+        self.reps = []
+
+
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ans = 0
-        seen = set()
-        rows, cols = len(grid), len(grid[0])
-        
-        def dfs(row, col):
-            if not( -1<row<rows and -1<col<cols) or grid[row][col]!=1 or (row,col) in seen:
+        visited = set()
+        def dfs(i, j):
+            if not(-1<i<len(grid) and -1<j<len(grid[0])) or grid[i][j]==0 or (i,j) in visited:
                 return 0
             
-            seen.add((row,col))
-            return 1 + dfs(row, col-1) + dfs(row-1, col) + dfs(row, col+1) + dfs(row+1, col)
+            visited.add((i,j))
+            ans = 1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j-1) + dfs(i, j+1)
+            
+            return ans
+        area = 0
         
-        for row in range(rows):
-            for col in range(cols):
-                if (row, col) not in seen:
-                    ans = max(ans, dfs(row,col))
-                    
-        return ans
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visited:
+                    area = max(dfs(i,j), area)
+        
+        return area
         
         
