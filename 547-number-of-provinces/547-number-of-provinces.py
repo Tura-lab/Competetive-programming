32826dc85bf3675ba436605339dd8245529659d4
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-26 23:46:48 +0300

    Time: 211 ms (77.43%), Space: 14.2 MB (77.24%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,20 +1,47 @@
+class UnionFind:
+    def __init__(self, n):
+        self.reps = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+        
+    def find(self, v):
+        if v == self.reps[v]:
+            return v
+        return self.find(self.reps[v])
+    
+    def union(self, v1, v2):
+        p1 = self.find(v1)
+        p2 = self.find(v2)
+        
+        if p1==p2:
+            return
+        
+        r1 = self.rank[p1]
+        r2 = self.rank[p2]
+        
+        if r1 == r2 or r1>r2:
+            self.rank[r1] +=r2
+            self.reps[p2] = p1
+        else:
+            self.rank[r2] +=r1
+            self.reps[p1] = p2
+
+            
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        seen = set()
-                    
-        def dfs(city):
-            seen.add(city)
-            for i, j in enumerate(isConnected[city]):
-                if i not in seen and j==1:
-                    dfs(i)
-                    
-        provinces = 0
-        for i in range(len(isConnected)):
-            if i not in seen:
-                dfs(i)
-                provinces +=1
-                
-        return provinces
+        n = len(isConnected)
+        components = UnionFind(n)
+        
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    components.union(i,j)
+        
+        count =0 
+        for i in range(n):
+            if components.reps[i] == i:
+                count+=1
+
+        return count
         
         
-        
+    
