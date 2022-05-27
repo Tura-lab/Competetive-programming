a8eefd53fae5afe2c3c4028884462e4b52c41932
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-27 18:17:12 +0300

    Time: 90 ms (10.29%), Space: 14 MB (74.08%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -3,19 +3,47 @@ class Solution:
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])
         
-        if obstacleGrid[-1][-1] == 1:
-            return 0
+        dp = [[0 for _ in range(col)] for k in range(row)]
+        dp[0][0] = 1 - obstacleGrid[0][0]
         
-        dp = [[-1 for _ in range(col)] for k in range(row)]
-        def dfs(i,j):
-            if i==row-1 and j==col-1:
-                return 1
-            if not (-1<i<row and -1<j<col) or obstacleGrid[i][j] == 1:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
+        for i in range(row):
+            for j in range(col):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                    continue
+                    
+                if -1<j-1<col:
+                    dp[i][j] += dp[i][j-1]
+                if -1<i-1<row:
+                    dp[i][j] += dp[i-1][j]
+        
+        return dp[-1][-1]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         row = len(obstacleGrid)
+#         col = len(obstacleGrid[0])
+        
+#         if obstacleGrid[-1][-1] == 1:
+#             return 0
+        
+#         dp = [[-1 for _ in range(col)] for k in range(row)]
+#         def dfs(i,j):
+#             if i==row-1 and j==col-1:
+#                 return 1
+#             if not (-1<i<row and -1<j<col) or obstacleGrid[i][j] == 1:
+#                 return 0
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
             
-            dp[i][j] = dfs(i+1,j) + dfs(i,j+1)
-            return dp[i][j]
+#             dp[i][j] = dfs(i+1,j) + dfs(i,j+1)
+#             return dp[i][j]
         
-        return dfs(0,0)
+#         return dfs(0,0)
