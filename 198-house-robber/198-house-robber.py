3f1c408fbd3566b3b86e47e66b24405ede14bfe9
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-29 21:43:53 +0300

    Time: 27 ms (97.12%), Space: 13.7 MB (97.52%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,83 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.cache = {}
-        
-    def ans(self,nums,i):
-        if i==len(nums)-3:
-            return nums[-3] + nums[-1]
-        elif i==len(nums)-2:
-            return nums[-2]
-        elif i==len(nums)-1:
-            return nums[-1]
-        if i+2 in self.cache:
-            # print('in')
-            first = self.cache[i+2] + nums[i]
-        else:
-            self.cache[i+2] = self.ans(nums, i+2)
-            first = self.cache[i+2] + nums[i]
-        if i+3 in self.cache:
-            # print('in')
-            second = self.cache[i+3] + nums[i]
-        else:
-            self.cache[i+3] = self.ans(nums, i+3)
-            second = self.cache[i+3] + nums[i]
-            
-        return max(first, second)
+    '''
+    1 2 3 1
+    
+    '''
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        first = self.ans(nums,0)
-        second = self.ans(nums,1)
-        return max(first,second)
+        dp = [-1 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        if len(nums)>1:
+            dp[1] = max(nums[0],nums[1])
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # i=0
-        # j=1
-        # s1 = nums[i]
-        # s2 = nums[j]
-        # while i<len(nums)-2:
-        #     if i==len(nums)-3:
-        #         print(nums[-1],end=',')
-        #         s1+=nums[-1]
-        #         break
-        #     if nums[i+3] > nums[i+2]:
-        #         s1+=nums[i+3]
-        #         print(nums[i+3], end=',')
-        #         i=i+3
-        #     else:
-        #         print(nums[i+2], end=',')
-        #         s1+=nums[i+2]
-        #         i=i+2
-        # print()
-        # while j<len(nums)-2:
-        #     if j==len(nums)-3:
-        #         print(nums[-1],end=',')
-        #         s2+=nums[-1]
-        #         break
-        #     if nums[j+3] > nums[j+2]:
-        #         print(nums[j+3], end=',')
-        #         s2+=nums[j+3]
-        #         j=j+3
-        #     else:
-        #         print(nums[j+2], end=',')
-        #         s2+=nums[j+2]
-        #         j=j+2
-        # print()
-        # return max(s1,s2)
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+            
+        return dp[-1]
