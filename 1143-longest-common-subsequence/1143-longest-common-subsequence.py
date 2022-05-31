796b9d38314e8ce879382b677ca4aec0a53f2086
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-31 12:09:00 +0300

    Time: 521 ms (61.92%), Space: 22 MB (65.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -12,7 +12,6 @@ class Solution:
         
         for i in range(1, n+1):
             for j in range(1, m+1):
-                print(j,i)
                 if text2[i-1] == text1[j-1]:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
