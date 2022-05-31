f5397a6a045bebd64e9018ba6467f0e4e831efb3
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-31 11:24:43 +0300

    Time: 1030 ms (21.46%), Space: 201.6 MB (5.34%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        # print(n,m)
+        
+        @lru_cache(1000000)
+        def dfs(i,j):
+            if i>=m or j>=n:
+                return 0
+            
+            if text1[i] == text2[j]:
+                return 1+dfs(i+1, j+1)
+            
+            return max(dfs(i+1,j), dfs(i,j+1))
+        
+        return dfs(0,0)
