2f06bb3775fd53cefe4ce80beb7d34a7e3060700
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-05-31 11:09:05 +0300

    Time: 7001 ms (74.34%), Space: 41.2 MB (63.38%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -4,14 +4,28 @@ class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
         # dp = [[None for _ in range(len(nums))] for _ in range(len(nums))]
         # print(len(multipliers), len(nums))
+        n, m = len(nums), len(multipliers)
+        dp = [[0]*(m+1) for _ in range(m+1)]
         
-        @lru_cache(2000)
-        def dfs(i,k):
-            j = len(nums)-1-(k-i)
-            if i>j or k>=len(multipliers):
-                return 0
+        for tot in range(m-1, -1, -1):
+            for i in range(tot, -1, -1):
+                j = n - 1 - (tot - i)
+                num = multipliers[tot]
+                
+                dp[tot][i] = max(nums[i]*num + dp[tot+1][i+1], nums[j]*num + dp[tot+1][i])
+                
+        return dp[0][0]
+        
+        
+        
+        
+#         @lru_cache(2000)
+#         def dfs(i,k):
+#             j = len(nums)-1-(k-i)
+#             if i>j or k>=len(multipliers):
+#                 return 0
 
-            return max(nums[i]*multipliers[k] + dfs(i+1,k+1), nums[j]*multipliers[k] + dfs(i,k+1))
+#             return max(nums[i]*multipliers[k] + dfs(i+1,k+1), nums[j]*multipliers[k] + dfs(i,k+1))
             
         
-        return dfs(0, 0)
+#         return dfs(0, 0)
