3d13e51323d5077ff9f5210e2af33e48f3fdd87d
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-01 21:36:52 +0300

    Time: 747 ms (45.33%), Space: 16.5 MB (86.07%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -29,10 +29,10 @@ class Solution:
             for j in range(1, col):
                 if matrix[i][j] == '1':
                     if dp[i][j-1]>0 and dp[i-1][j-1]>0 and dp[i-1][j]>0:
-                        dp[i][j] = int(((min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])**0.5)+1)**2)
+                        dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])
                     else:
                         dp[i][j] = 1
                     
                     ans = max(ans, dp[i][j])
                         
-        return ans
+        return ans*ans
