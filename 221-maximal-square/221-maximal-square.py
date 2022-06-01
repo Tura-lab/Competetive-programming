2bbc312cd61989e0880e644d598bc946f5298943
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-01 21:33:41 +0300

    Time: 985 ms (19.35%), Space: 18.9 MB (17.65%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.py
@@ -0,0 +1,38 @@
+class Solution:
+    '''
+    [["1","1","1","1","0"],
+     ["1","1","1","1","0"],
+     ["1","1","1","1","1"],
+     ["1","1","1","1","1"],
+     ["0","0","1","1","1"]]
+     
+     [[1, 1, 1, 1, 0], 
+      [1, 4, 4, 4, 0], 
+      [1, 4, 16, 16, 1], 
+      [1, 4, 16, 64, 4], 
+      [0, 0, 1, 4, 16]]
+
+
+      '''
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        row, col = len(matrix), len(matrix[0])
+        dp = [[0]*col for _ in range(row)]
+        
+        ans = 0
+        for i in range(row):
+            for j in range(col):
+                if i==0 or j==0:
+                    dp[i][j] = int(matrix[i][j])
+                    ans = max(ans, dp[i][j])
+        
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[i][j] == '1':
+                    if dp[i][j-1]>0 and dp[i-1][j-1]>0 and dp[i-1][j]>0:
+                        dp[i][j] = int(((min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])**0.5)+1)**2)
+                    else:
+                        dp[i][j] = 1
+                    
+                    ans = max(ans, dp[i][j])
+                        
+        return ans
