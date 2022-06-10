4d80acaf5ef209acd143a542c36a4a681451ac3b
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-10 13:23:48 +0300

    Time: 93 ms (13.38%), Space: 14.1 MB (76.10%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,36 +1,75 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack = []
-        found = set()
-
-        for i in range(len(s)):
-            
-            if s[i] == '(':
-                stack.append(i)
-            
-            else:
-                if stack:
-                    j = stack.pop()
-                    found.add((j,i))
-                    if (j+1, i-1) in found:
-                        found.remove((j+1, i-1))
         
-        new = []
+        dp = [0]*len(s)
         ans = 0
-        found = sorted(list(found), key = lambda x: x[0])
-        for i,j in found:
-            if not new:
-                new.append([i,j])
-            elif new[-1][1] +1== i:
-                new[-1][1] = j
-            elif new[-1][1] +1 > i:
-                if j-i > new[-1][1]-new[-1][0]:
-                    new.pop()
-                    new.append([i,j])                    
-            else:
-                new.append([i,j])
-            
-            ans = max(ans, new[-1][1] - new[-1][0] +1)
+        
+        for i in range(len(s)):
+            if s[i] == ')' and i>0:
+                if s[i-1]=='(':
+                    dp[i] = 2
+                    if i-2>=0:
+                        dp[i] += dp[i-2]
+
                 
-        # print(found)
+                elif s[i-1]==')' and i-dp[i-1]-1>=0 and s[i-dp[i-1]-1] == '(':
+                    dp[i] = 2 + dp[i-1] 
+                    if i-dp[i-1]-2 >=0:
+                        dp[i] += dp[i-dp[i-1]-2]
+            
+            ans = max(ans, dp[i])
+            
         return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         stack = []
+#         found = set()
+
+#         for i in range(len(s)):
+            
+#             if s[i] == '(':
+#                 stack.append(i)
+            
+#             else:
+#                 if stack:
+#                     j = stack.pop()
+#                     found.add((j,i))
+#                     if (j+1, i-1) in found:
+#                         found.remove((j+1, i-1))
+        
+#         new = []
+#         ans = 0
+#         found = sorted(list(found), key = lambda x: x[0])
+#         for i,j in found:
+#             if not new:
+#                 new.append([i,j])
+#             elif new[-1][1] +1== i:
+#                 new[-1][1] = j
+#             elif new[-1][1] +1 > i:
+#                 if j-i > new[-1][1]-new[-1][0]:
+#                     new.pop()
+#                     new.append([i,j])                    
+#             else:
+#                 new.append([i,j])
+            
+#             ans = max(ans, new[-1][1] - new[-1][0] +1)
+                
+#         # print(found)
+#         return ans
