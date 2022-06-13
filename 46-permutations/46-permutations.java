fad6a69c76dcfdb077a689f7b0d17a49d00d2e37
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-13 16:44:27 +0300

    Time: 4 ms (20.09%), Space: 44 MB (71.03%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,25 +1,28 @@
 class Solution {
     
-    public static void permutations(int[] nums, int size, List<Integer> yet, List<List<Integer>> ans){
+    public static void permutations(int[] nums, int size, List<Integer> yet, List<List<Integer>> ans, Set<Integer> seen){
         if (yet.size() == nums.length){ 
             ans.add(new ArrayList<Integer>(yet)); 
         }
-        
         for (int num: nums){
-            if (!yet.contains(num)){
+            if (!seen.contains(num)){
+                // Choose 
                 yet.add(num);
-                permutations(nums, size, yet, ans);
+                seen.add(num);
+                
+                // Explore
+                permutations(nums, size, yet, ans, seen);
+                
+                // Unchoose
+                seen.remove(num);
                 yet.remove(yet.size()-1);
             }
         }
     }
     
-    
     public List<List<Integer>> permute(int[] nums) {
-        
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        
-        permutations(nums, 0, new ArrayList<Integer>(), ans);
+        permutations(nums, 0, new ArrayList<Integer>(), ans, new HashSet<Integer>());
         
         return ans;
         
