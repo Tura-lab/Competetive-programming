03436325bfd35c59b08e03641cae750d1b60b88a
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-15 22:24:44 +0300

    Time: 402 ms (5.46%), Space: 18.2 MB (6.13%) - LeetHub

--- 372-super-pow/372-super-pow.py
+++ 372-super-pow/372-super-pow.py
@@ -1,5 +1,21 @@
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
-        print(len(b))
-        num = ''.join(map(str, b))
-        return pow(a, int(num), 1337)
+        num = int(''.join(map(str, b)))
+        a = a%1337
+        
+        def p(num):
+            if num<=1:
+                return a
+            
+            ans = p(num//2)%1337
+            
+            if num%2==0:
+                return ((ans) * (ans))%1337
+        
+        
+            return ((a)*(ans)*(ans))%1337
+            
+                
+            
+        return p(num)
+        # return pow(a, num, 1337)
