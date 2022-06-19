0dd3050cede155a74b284e083e68a16d02ad8c43
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-19 15:34:37 +0300

    Time: 366 ms (20.08%), Space: 37.4 MB (8.18%) - LeetHub

--- 648-replace-words/648-replace-words.py
+++ 648-replace-words/648-replace-words.py
@@ -1,25 +1,53 @@
+class TrieNode:
+    def __init__(self, val=None):
+        self.val = val 
+        self.children = [None]*26
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for i in range(len(word)):
+            c = word[i]
+            if node.children[ord(c)-ord('a')] == None:
+                n = TrieNode(val=c)
+                node.children[ord(c)-ord('a')] = n
+            node = node.children[ord(c)-ord('a')]
+        node.isEnd=True
+    
+    def traverse(self, prefix):
+        node = self.root
+        j=0
+        for i in range(len(prefix)):
+            c = prefix[i]
+            if not node.children[ord(c) - ord('a')] or node.children[ord(c) - ord('a')].val != c:
+                return j if node.isEnd else 0
+            
+            if node.isEnd:
+                return j;
+            
+            node = node.children[ord(c) - ord('a')]
+            j+=1
+        
+        return j
+
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        hashes = {}
-        def hash(word):
-            code = 0
-            for i in range(len(word)):
-                code += (26**i)*(ord(word[i])-ord('a') + 1)
-                
-            return code
-
+        trie = Trie()
         for word in dictionary:
-            hashes[hash(word)] = word
-            
+            trie.insert(word)
+
         s = sentence.split()
         for i in range(len(s)):
-            code = 0
-            for j in range(0,len(s[i])):
-                code += (26**j)*(ord(s[i][j])-ord('a') + 1)
-                if code in hashes:
-                    s[i] = hashes[code]
-                    break
-                    
+            word = s[i]
+            ans = trie.traverse(word)
+            if ans !=0:
+                s[i] = s[i][:ans]
+            
         return ' '.join(s)
                     
                 
