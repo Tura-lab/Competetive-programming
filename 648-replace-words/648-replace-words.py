66b397822b1e461fb828fe415b5dca6506e2bea6
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-06-19 15:12:11 +0300

    Time: 1337 ms (5.42%), Space: 18.5 MB (82.51%) - LeetHub

--- 648-replace-words/648-replace-words.py
+++ 648-replace-words/648-replace-words.py
@@ -1,6 +1,5 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        mod = 10**9 + 7
         hashes = {}
         def hash(word):
             code = 0
@@ -12,9 +11,7 @@ class Solution:
         for word in dictionary:
             hashes[hash(word)] = word
             
-            
         s = sentence.split()
-        
         for i in range(len(s)):
             code = 0
             for j in range(0,len(s[i])):
