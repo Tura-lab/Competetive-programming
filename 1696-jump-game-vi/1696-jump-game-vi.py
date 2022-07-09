eee7e892773db48bedec97880eadd150d66ded27
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-09 11:20:59 +0300

    Time: 2075 ms (18.25%), Space: 33.5 MB (30.03%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,67 +1,72 @@
-'''
-  1 -1 -2  4  -7  3
-  0  6    
-  
-'''
-from collections import deque
+from heapq import heappush, heappop
 
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        if len(nums)<3:
-            return sum(nums)
-        ans = deque()
+        n = len(nums)
+        # print(n)
+        # dp = [-1]*n
         
-        ans.appendleft(len(nums)-1)
-        val = nums[-2]+nums[-1]
-        nums[-2]= val
-        
-        while ans and nums[ans[0]] < val:
-            ans.popleft()
-        ans.appendleft(len(nums)-2)
-        
-        i=len(nums)-3
-        while i>-1:
-            while ans[-1]-i>k:
-                ans.pop()
-                
-            val = nums[ans[-1]] + nums[i]
-            nums[i] = val
+        pos = []
+
+        for i in range(n-1, -1, -1):
+
+            while pos and pos[0][1] > i+k:
+                heapq.heappop(pos)
+
+            ans = -nums[i] if not pos else pos[0][0] - nums[i]
+            heapq.heappush(pos, (ans, i))
             
-            while ans and nums[ans[0]]<val:
-                ans.popleft()
-            ans.appendleft(i)            
+        # print(pos)
+        return -ans
+
+    
+    
+
+
+
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         @cache
+#         def solve(i):
+#             if i==n-1:
+#                 return nums[i]
             
-            i-=1
-        return val
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         TLE
-#         if len(nums)<3:
-#             return sum(nums)
-        
-#         dp = [-(10)**5]*(len(nums)+1)
-#         def helper(end, cur): 
-#             if dp[end] != -(10)**5:
-#                 return dp[end]
-#             if cur == len(nums)-1:
-#                 return 0
+#             ans = -float('inf')
+#             for j in range(i+1, min(i+k+1, n)):
+#                 ans = max(ans, nums[i] + solve(j))
+
+#             return ans
             
-#             pick = nums[cur] + helper(cur, cur+1)
-#             if cur-end ==k:
-#                 dp[end] = pick
-#                 return dp[end]
-#             dont = helper(end, cur+1)
-            
-#             dp[end] = max(pick, dont)
-            
-#             return dp[end]
-#         print(len(nums))
-#         return helper(0,1) + nums[0] + nums[-1]
+#         return solve(0)
