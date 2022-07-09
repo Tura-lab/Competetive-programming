53291d9748afd972a9187c61e0a8c46ee0b7cce4
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-10 00:33:49 +0300

    Time: 6186 ms (6.62%), Space: 165.2 MB (9.99%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # print(len(prices))
+        
+        # The problem is litterally the same as the IVth variant but this time k==2
+        
+        k = 2
+        dp = [[[-1 for _ in range(k+1)] for __ in range(2)] for ___ in range(len(prices)+1)]
+        def solve(i, last, made_yet):
+            if i == len(prices) or made_yet==k:
+                return 0
+            
+            # print(i, last, made_yet)
+            if dp[i][last][made_yet] == -1:
+                
+                if last == 0:
+                    buy = -prices[i] + solve(i+1, 1, made_yet)
+                    dont_buy = solve(i+1, 0, made_yet)
+                    dp[i][last][made_yet] = max(buy, dont_buy)
+                else:
+                    sell = prices[i] + solve(i+1, 0, made_yet+1)
+                    dont_sell = solve(i+1, 1, made_yet)
+                    dp[i][last][made_yet] = max(sell, dont_sell)
+
+            return dp[i][last][made_yet]
+        
+        return solve(0, 0, 0)
