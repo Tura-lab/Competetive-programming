e63b19bf3cab054c9e93eafa89e7f16b58d18673
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-10 17:13:18 +0300

    Time: 423 ms (16.14%), Space: 14.8 MB (53.26%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.py
+++ 707-design-linked-list/707-design-linked-list.py
@@ -1,116 +1,79 @@
-class ListNode:
-    def __init__(self, val=None, next=None):
-        self.val = val
-        self.next = next
-        
 class MyLinkedList:
+
     def __init__(self):
         self.head = None
+        
+
     def get(self, index: int) -> int:
-        # print('get')
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        temp = self.head
-        i=0
-        while temp:
-            if i == index:
-                return temp.val
-            temp = temp.next
-            i+=1
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        return -1
+        node = self.head
+        while index and node:
+            node = node.next
+            index-=1
+            
+        return -1 if not node else node.val
+        
+        
     def addAtHead(self, val: int) -> None:
-        # print('addAtHead')
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        nxt = self.head
-        self.head = ListNode(val, nxt)
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-    def addAtTail(self, val: int) -> None:
-        if self.head ==None:
+        if not self.head:
             self.head = ListNode(val)
             return
-        # print('addAtTail')
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        temp = self.head
-        while temp and temp.next:
-            temp = temp.next
-        temp.next = ListNode(val)
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
+        
+        node = ListNode(val)
+        node.next = self.head
+        self.head = node
+        
+
+    def addAtTail(self, val: int) -> None:
+        if not self.head:
+            self.head = ListNode(val)
+            return
+        node = self.head
+        while node.next:
+            node = node.next
+        
+        node.next = ListNode(val)
+        
+
     def addAtIndex(self, index: int, val: int) -> None:
-        # print('addAtindex')
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        if index ==0:
-            self.head = ListNode(val, self.head)
+        if not self.head:
+            if index != 0: return
+            self.head = ListNode(val)
             return
-        i=0
-        temp=self.head
-        while temp:
-            if i == index-1:
-                temp.next = ListNode(val, temp.next)
-                break
-            temp = temp.next
-            i+=1
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
+        
+        if index == 0:
+            node = ListNode(val)
+            node.next = self.head
+            self.head = node
+            return
+                
+        node = self.head
+        while index-1 and node:
+            node = node.next
+            index -=1
+        
+        if not node: return 
+        new = ListNode(val)
+        new.next = node.next
+        node.next = new
+        
+
     def deleteAtIndex(self, index: int) -> None:
-        if self.head == None:
-            return
-        if index==0:
+        if index == 0:
             self.head = self.head.next
             return
-        # print('deleteAtIndex')
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
-        i=0
-        temp = self.head
-        while temp:
-            if i==index-1:
-                if temp.next:
-                    temp.next = temp.next.next
-                else:
-                    temp.next = None
-                break
-            temp = temp.next
-            i+=1
-        # t = self.head
-        # while t:
-        #     print(t.val, end= '*')
-        #     t = t.next
-        # print()
         
+        node = self.head
+        if not node:
+            return
+        
+        while index-1 and node:
+            node = node.next
+            index -= 1
+        
+        if not node: return
+        node.next = None if not node.next else node.next.next
+        
+
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
