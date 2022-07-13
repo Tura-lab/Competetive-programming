9763717324ae53d1723833a41145c24304c39de6
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-13 14:14:11 +0300

    Time: 106 ms (55.38%), Space: 15.2 MB (24.50%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hashmap:
+                return [hashmap[target - nums[i]], i]
+            hashmap[nums[i]] = i
