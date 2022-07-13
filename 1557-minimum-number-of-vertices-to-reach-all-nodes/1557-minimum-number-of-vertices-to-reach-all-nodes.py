8ec7b9d8920c4da51e4bcd7e9980079896424f0a
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-13 19:29:31 +0300

    Time: 1686 ms (57.12%), Space: 52.3 MB (68.39%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,25 +1,33 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = [[] for _ in range(n)]
+        indeg = [0]*n
         
-        visited = set()
-        ans = set()
         for a, b in edges:
-            graph[a].append(b)
+            indeg[b] +=1
+        
+        return [i for i in range(len(indeg)) if indeg[i]==0]
+        
+        
+#         graph = [[] for _ in range(n)]
+        
+#         visited = set()
+#         ans = set()
+#         for a, b in edges:
+#             graph[a].append(b)
             
-        def dfs(node):
-            if node in visited:
-                return 
-            visited.add(node)
-            ans.add(i)
+#         def dfs(node):
+#             if node in visited:
+#                 return 
+#             visited.add(node)
+#             ans.add(i)
             
-            for v in graph[node]:
-                if v in ans:
-                    ans.remove(v)
-                dfs(v)
+#             for v in graph[node]:
+#                 if v in ans:
+#                     ans.remove(v)
+#                 dfs(v)
             
             
-        for i in range(n):
-            dfs(i)
+#         for i in range(n):
+#             dfs(i)
             
-        return ans
+#         return ans
