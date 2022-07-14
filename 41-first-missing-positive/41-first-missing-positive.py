6ad5dd57a74406ced4e9e8958e45a37777d05c73
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-07-14 18:28:45 +0300

    Time: 932 ms (90.59%), Space: 63.2 MB (48.81%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,49 +1,19 @@
 class Solution:
     '''
-    1,-1,3,4
+     1  2  3 4
+     1 -1  3 4
     '''
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
-            while 0 < nums[i] < len(nums)+1 and i != nums[i]-1 and nums[nums[i]-1] != nums[i]:
-                val = nums[nums[i]-1]
-                nums[nums[i]-1] = nums[i]
-                nums[i]=val   
-            i+=1
-        
-        for i in range(1, len(nums)+1):
-            if nums[i-1] != i:
-                return i
-        return i+1
+            while -1<nums[i]-1<len(nums) and i!=nums[i]-1 and nums[nums[i]-1]!=nums[i]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
                 
+        # print(nums)
         
-#         seen = set()
-#         missing = 1
+        ans = 1
+        for num in nums:
+            if num != ans:
+                return ans
+            ans+=1
         
-#         for num in nums:
-#             if num == missing:
-#                 missing+=1
-#                 while missing in seen:
-#                     missing+=1
-#             seen.add(num)
-                
-#         return missing
-
-        
-        
-        
-        
-        
-#         nums.sort()
-#         for i in range(len(nums)):
-#             if nums[i] >= 0:
-#                 break
-                
-#         missing = 1
-#         for j in range(i, len(nums)):
-#             if nums[j]>missing:
-#                 return missing
-#             if nums[j] == missing:
-#                 missing = nums[j]+1
-        
-#         return missing
-        
+        return ans
