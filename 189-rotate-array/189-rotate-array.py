66aed70c667fc1cc38fce629aa594b3cfaefeb41
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-08-11 01:50:09 +0300

    Time: 221 ms (95.65%), Space: 25.3 MB (75.84%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,7 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
+        k, n = k%len(nums), len(nums)
         
-        if k>0:
-            nums[:-k], nums[-k:] = nums[-k:], nums[:-k]
+        def rotate(i,j):
+            while i<j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+                j-=1
+        
+        # Reverse the first n-k elements
+        rotate(0, n-k-1)
+        
+        # Reverse the rest of the array
+        rotate(n-k, n-1)
+        
+        # Reverse the whole array
+        rotate(0,n-1)
