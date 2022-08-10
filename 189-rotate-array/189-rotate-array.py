0cdd171523bc2a2a350bcf38795081ecee8c758c
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-08-11 01:42:23 +0300

    Time: 233 ms (90.42%), Space: 25.6 MB (6.57%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -2,22 +2,8 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        5 6 7 4 1 2 3,     k=3
         """
-        #O(1) space
-        k = k%len(nums)
+        k %= len(nums)
+        
         if k>0:
             nums[:-k], nums[-k:] = nums[-k:], nums[:-k]
-        return
-        #O(n) space
-        new = []
-        k = len(nums)-k
-        while k<len(nums):
-            new.append(nums[k])
-            k+=1
-        i=0
-        while len(new)<len(nums):
-            new.append(nums[i])
-            i+=1
-        for i in range(len(new)):
-            nums[i] = new[i]
