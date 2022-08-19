f83997ff61d6a30b56e4e46cfe204cc65ca7d449
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-08-19 10:55:44 +0300

    Time: 141 ms (44.64%), Space: 15.3 MB (92.61%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,30 +1,31 @@
 class Solution:
-    def searchRange(self, nums, target):
-        found = [None, None]
-        l=0
-        r = len(nums)-1
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first = last = -1
         
-        def bs(l ,r):
-            while l<=r:
-                mid = (l+r)//2
-                if nums[mid] == target:
-                    if found[0] == None:
-                        found[0] = mid
-                    else:
-                        found[0] = min(found[0], mid)
-                    if found[1] == None:
-                        found[1] = mid
-                    else:
-                        found[1] = max(found[1], mid)
-                    bs(l,mid-1)
-                    bs(mid+1,r)
-                    return
-                elif nums[mid] < target:
-                    l = mid+1
-                else:
-                    r = mid-1
-        bs(0, len(nums)-1)
-        if found == [None, None]:
-            return [-1,-1]
-        return found
+        l=0
+        r=len(nums)-1
+        
+        while l<=r:
+            mid = (l+r)//2
             
+            if nums[mid] >= target:
+                r= mid-1
+                if nums[mid] == target:
+                    first = mid
+            else:
+                l=mid+1
+        
+        l=0
+        r=len(nums)-1
+        
+        while l<=r:
+            mid = (l+r)//2
+            
+            if nums[mid] <= target:
+                l= mid+1
+                if nums[mid] == target:
+                    last = mid
+            else:
+                r=mid-1
+        
+        return  [first,last]
