67aacf95243bfc0607c0cfba22a4204fe5c65f33
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-08-24 23:13:06 +0300

    Time: 133 ms (74.85%), Space: 14.3 MB (47.61%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,83 +1,24 @@
-# from functools import lru_cache
-
 class Solution:
-    '''
-    10,9,2,5,3,7,101,18
-    2 3 7 101 
-    '''
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1]*len(nums)
+        ans = []
         
-        _max = 1
-        for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
-                    _max = max(dp[i], _max)
+        for num in nums:
+            if not ans or ans[-1]<num:
+                ans.append(num)
+            else:
+                l = 0
+                r = len(ans)-1
+                
+                pos=0
+                while l<=r:
+                    mid = l + (r-l)//2
                     
-        return _max
-            
-            
+                    if ans[mid] >= num:
+                        pos = mid
+                        r = mid-1
+                    else:
+                        l = mid+1
         
+                ans[pos] = num
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         #Binary search to find the correct index of replacement
-#         def findPosAndReplace(num,s, e):
-#             mid = (s+e)//2
-#             if arr[mid] >= num and (mid-1<0 or arr[mid-1] < num):
-#                 arr[mid] = num
-#                 return
-#             if arr[mid] > num:
-#                 findPosAndReplace(num,s,mid)
-#             else:
-#                 findPosAndReplace(num,mid,e)
-        
-#         arr = []
-#         for num in nums:
-#             if arr==[] or num>arr[-1]:
-#                 arr.append(num)
-#             else:
-#                 findPosAndReplace(num,0,len(arr))
-#         return len(arr)
-    
-#     # DP (TLE)
-#     def lengthOfLIS(self, nums: List[int]) -> int:
-#         if len(nums) == 1:
-#             return 1
-        
-#         cache = [-1]*(len(nums)+1)
-#         def helper(end, i):
-#             if i >= len(nums):
-#                 return 0
-#             if cache[end+1] !=-1:
-#                 return cache[end+1]
-#             dont = helper(end, i+1)
-#             pick = 0
-#             if end==-1 or nums[i]>nums[end]:
-#                 pick = 1+helper(i, i+1)
-#             cache[end+1] = max(pick, dont)
-#             return cache[end+1]
-#         # print(cache)
-#         return helper(-1,0)
+        return len(ans)
