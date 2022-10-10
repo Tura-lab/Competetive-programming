38b2cbad43fa6ababbd269acdc50fb597b412423
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-10 23:03:23 +0300

    Time: 4122 ms (40.69%), Space: 145.5 MB (86.08%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -5,26 +5,19 @@ class Solution:
         
         for i in range(len(parent)):
             if parent[i] == -1:continue
-                
             tree[parent[i]].append(i)
             
             
         self.ans = 0
         def dfs(node, p):
-            
-            heap = []
+            a = b = 0
             for v in tree[node]:
-                heapq.heappush(heap, -dfs(v,node))
-              
-            if len(heap)==0:
-                a = 0
-                b = 0
-            elif len(heap) == 1:
-                a = -heappop(heap)
-                b = 0
-            else:
-                a = -heappop(heap)
-                b = -heappop(heap)
+                cur = dfs(v, node)
+                if cur > a:
+                    b = a
+                    a = cur
+                elif cur > b:
+                    b  = cur
                 
             self.ans = max(self.ans, 1+a+b)
             
