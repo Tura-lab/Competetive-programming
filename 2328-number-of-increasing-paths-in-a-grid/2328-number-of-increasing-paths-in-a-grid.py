7dd3b8fa1f10f0ac3922ca46a14775ff419d56dd
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-12 23:51:40 +0300

    Time: 2532 ms (93.70%), Space: 76.5 MB (64.76%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -6,19 +6,19 @@ class Solution:
         '''
         mod = 10**9 +7
         r,c = len(grid),len(grid[0])
-        found = {}
+        found = [[-1]*c for _ in range(r)]
         
         def dfs(i,j):
-            if (i,j) not in found:
+            if found[i][j] == -1:
                 count = 0
                 for x,y in [[1,0],[0,1],[-1,0],[0,-1]]:
                     a,b = x+i,y+j
                     if -1<a<r and -1<b<c and grid[a][b] > grid[i][j]:
                         count += 1 + dfs(a,b)
                 
-                found[(i,j)] = count
+                found[i][j] = count
             
-            return found[(i,j)]
+            return found[i][j]
         
         tot = 0
         for i in range(r):
