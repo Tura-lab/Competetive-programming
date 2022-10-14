40680815ba5e38557f98ddbc9720d7e1d461902a
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-14 18:42:42 +0300

    Time: 2123 ms (29.82%), Space: 49 MB (15.56%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,29 +1,29 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        answer = [set() for _ in range(n)]
+        parents = [set() for _ in range(n)]
+        graph = [[] for _ in range(n)]
+        indeg = [0]*n
         
-        incomming = {i:0 for i in range(n)}
-        outgoing = {i:set() for i in range(n)}
+        for a,b in edges:
+            graph[a].append(b)
+            indeg[b] += 1
         
-        for fro, to in edges:
-            incomming[to] +=1
-            outgoing[fro].add(to)
-            
-        q = collections.deque()
+        q = deque()
+        for i in range(n):
+            if indeg[i] == 0:
+                q.append(i)
         
-        for key in incomming.keys():
-            if incomming[key] == 0:
-                q.append(key)
-                
         while q:
-            node = q.popleft()
+            for _ in range(len(q)):
+                node = q.popleft()
+                for v in graph[node]:
+                    parents[v].add(node)
+                    for x in parents[node]:
+                        parents[v].add(x)
+                    indeg[v] -= 1
+                    if indeg[v] == 0:
+                        q.append(v)
+                    
             
-            for v in outgoing[node]:
-                answer[v].update(answer[node])
-                answer[v].add(node)
-                incomming[v] -=1
-                if incomming[v] ==0:
-                    q.append(v)
-        
-
-        return [sorted(ans) for ans in answer]
+            
+        return [sorted(list(a)) for a in parents]
