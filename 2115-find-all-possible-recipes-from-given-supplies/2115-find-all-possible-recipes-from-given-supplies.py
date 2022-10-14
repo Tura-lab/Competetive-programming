ac0bf13c06a66fb4ba3a5c4903011ef42e3b8798
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-14 15:37:07 +0300

    Time: 4225 ms (5.64%), Space: 17 MB (67.26%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,46 +1,18 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         supplies = set(supplies)
-        outgoing = {}
-        incomming = {}
-        
-        for i in range(len(recipes)):
-            recipe = recipes[i]
-            for ing in ingredients[i]:
-                if recipe not in incomming:
-                    incomming[recipe] = 1
-                else:
-                    incomming[recipe] += 1
-                if ing not in incomming:
-                    incomming[ing] = 0                
-                    
-                if recipe not in outgoing:
-                    outgoing[recipe] = set()
-                if ing not in outgoing:
-                    outgoing[ing] = {recipe}
-                else:
-                    outgoing[ing].add(recipe)
-        
-        recipes = set(recipes)
-        for key in incomming.keys():
-            if key not in recipes and key not in supplies:
-                incomming[key] +=1
-        q = collections.deque()
-        for key in incomming.keys():
-            if incomming[key] == 0:
-                q.append(key)
+        def check(i):
+            for a in ingredients[i]:
+                if a not in supplies:
+                    return False
                 
-        top_sort = []
+            return True
         
-        while q:
-            node = q.popleft()
-            
-            for v in outgoing[node]:
-                incomming[v]-=1
-                if incomming[v] == 0:
-                    q.append(v)
-            
-            if node in recipes:
-                top_sort.append(node)
-            
-        return top_sort
+        for _ in range(len(recipes)):
+            for i in range(len(recipes)):
+                if recipes[i]not in supplies and check(i):
+                    supplies.add(recipes[i])
+                    
+        return [a for a in recipes if a in supplies]
+    
+    
