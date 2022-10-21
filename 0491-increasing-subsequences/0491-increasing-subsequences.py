2ad1fce224fdac3a7c1eab8f55ce3c6251f9c956
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-21 16:44:24 +0300

    Time: 239 ms (89.04%), Space: 22.6 MB (24.28%) - LeetHub

--- 0491-increasing-subsequences/0491-increasing-subsequences.py
+++ 0491-increasing-subsequences/0491-increasing-subsequences.py
@@ -2,18 +2,20 @@ class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = set()
+        
         path = []
         def dfs(i):
-            if i == n:
-                if len(path)>1:
-                    ans.add(tuple(path))
-                return 
+            if len(path) >= 2 and path[-1] < path[-2]:
+                return
             
-            if not path or path[-1] <= nums[i]:
-                path.append(nums[i])
-                dfs(i+1)
+            if len(path) >= 2:
+                ans.add(tuple(path[:]))
+                
+            for j in range(i, n):
+                path.append(nums[j])
+                dfs(j+1)
                 path.pop()
-            dfs(i+1)
-            
+                
         dfs(0)
         return ans
+        
