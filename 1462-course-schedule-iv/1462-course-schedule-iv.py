36c2bb225979a47fb937b98a8ecb00c5a58ba2cc
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-10-27 11:03:35 +0300

    Time: 2751 ms (29.32%), Space: 17.7 MB (42.79%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,29 +1,15 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        parents = [set() for _ in range(numCourses)]
-        indeg = [0]*numCourses
-        graph = [[]for _ in range(numCourses)]
+        dp = [[False] * numCourses for _ in range(numCourses)]
         
         for a,b in prerequisites:
-            graph[a].append(b)
-            indeg[b] += 1
-            
-        q = deque()
-        for i in range(numCourses):
-            if indeg[i] == 0:
-                q.append(i)
-                
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                
-                for v in graph[node]:
-                    parents[v].add(node)
-                    for x in parents[node]:
-                        parents[v].add(x)
-                    
-                    indeg[v] -=1
-                    if indeg[v] == 0:
-                        q.append(v)
+            dp[a][b] = True
         
-        return  [u in parents[v] for u,v in queries]
+        for k in range(numCourses):
+            for j in range(numCourses):
+                for i in range(numCourses):
+                    if i==j or j==k or i == k:continue
+                    dp[i][j] = dp[i][j] or (dp[i][k] and dp[k][j])
+        
+        return [dp[a][b] for a,b in queries]
+            
