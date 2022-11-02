5c3d7b7468631d999d09e85d0abe044a9b9c8256
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-11-02 15:59:16 +0300

    Time: 134 ms (43.01%), Space: 14 MB (57.53%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -3,19 +3,27 @@ class Solution:
         self.ans = 0
         r,c = len(grid),len(grid[0])
         
-        total = r*c
+        total = (1 << ((r*c))) -1
         start = None
+        
+        nums = {}
+        count = 0
+        for i in range(r):
+            for j in range(c):
+                nums[(i,j)] = count
+                count += 1
+        
         
         for i in range(r):
             for j in range(c):
                 if grid[i][j] == -1:
-                    total -= 1
+                    total ^= 1 << nums[(i,j)]
                 if grid[i][j] == 1:
                     start = (i,j)
         
-        def dfs(i,j):
+        def dfs(i,j,mask):
             # print(path)
-            if len(visited) == total:
+            if mask == total:
                 self.ans += grid[i][j] == 2
                 return
             if grid[i][j] == 2:
@@ -23,16 +31,12 @@ class Solution:
             
             for x,y in [[1,0],[-1,0],[0,1],[0,-1]]:
                 nx,ny = i+x, j+y
-                if -1<nx<r and -1<ny<c and grid[nx][ny] != -1 and (nx,ny) not in visited:
-                    visited.add((nx,ny))
+                if -1<nx<r and -1<ny<c and grid[nx][ny] != -1 and (1 << nums[(nx,ny)]) & mask == 0:
                     # path.append((nx,ny))
-                    dfs(nx,ny)
+                    dfs(nx, ny, mask | (1 << nums[(nx,ny)]))
                     # path.pop()
-                    visited.remove((nx,ny))        
-            
             
         # path = [(start[0], start[1])]
-        visited = set([(start[0], start[1])])
-        dfs(start[0], start[1])
+        dfs(start[0], start[1], 1 << nums[(start[0], start[1])])
         return self.ans
     
