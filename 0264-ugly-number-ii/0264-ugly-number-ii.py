a19a734d1aadcf1116bea02773248c66d21434dc
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-11-04 22:23:28 +0300

    Time: 375 ms (49.35%), Space: 14 MB (55.39%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,10 +1,18 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        nums = []
-        for a in range(31):
-            for b in range(20):
-                for c in range(14):
-                    nums.append(2**a * 3**b * 5**c)
-    
-        nums.sort()
-        return nums[n-1]
+        a,b,c = 2,3,5
+        a1, b1, c1 = 0,0,0
+        nums = [1]
+        
+        while len(nums) < n:
+            val = min(nums[a1]*a, nums[b1]*b, nums[c1]*c)
+            nums.append(val)
+            
+            if val == nums[a1]*a:
+                a1 += 1
+            if val == nums[b1]*b:
+                b1 += 1
+            if val == nums[c1]*c:
+                c1 += 1
+                        
+        return nums[-1]
