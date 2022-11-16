c261cf115e860684140f83322a40cc8662201ce3
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-11-16 18:27:18 +0300

    Time: 281 ms (46.53%), Space: 14.3 MB (12.46%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,11 +1,49 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        ans = 0
+        n = len(nums)
+        mod = 10 ** 9 + 7
         
-        for i in range(len(nums)):
-            mn = mx = nums[i]
-            for j in range(i+1, len(nums)):
-                mn = min(mn, nums[j])
-                mx = max(mx, nums[j])
-                ans += mx-mn
+        left  = [0] * n
+        right = [n-1] * n
+        
+        stack = []
+        for i in range(n):
+            while stack and stack[-1][0] < nums[i]:
+                num, idx = stack.pop()
+                right[idx] = i-1
+            stack.append((nums[i], i))
+            
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and stack[-1][0] <= nums[i]:
+                num, idx = stack.pop()
+                left[idx] = i+1
+            stack.append((nums[i], i))
+            
+        ans = 0
+        for i in range(n):
+            ans += (i - left[i] + 1) * (right[i] - i + 1) * nums[i]
+        
+
+        left  = [0] * n
+        right = [n-1] * n
+        
+        stack = []
+        for i in range(n):
+            while stack and stack[-1][0] > nums[i]:
+                num, idx = stack.pop()
+                right[idx] = i-1
+            stack.append((nums[i], i))
+            
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and stack[-1][0] >= nums[i]:
+                num, idx = stack.pop()
+                left[idx] = i+1
+            stack.append((nums[i], i))
+            
+        for i in range(n):
+            ans -= (i - left[i] + 1) * (right[i] - i + 1) * nums[i]
+        
+        
         return ans
