981c88b8b68dcedb58e28e88b59cc0ec2bca1415
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-11-25 10:37:18 +0300

    Time: 884 ms (12.42%), Space: 86.8 MB (5.11%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,21 +1,22 @@
 class Solution:
-    def canReach(self, arr: List[int], start: int) -> bool:
-        q = collections.deque()
-        q.append(start)
-        n = len(arr)
-        seen = set()
-        seen.add(start)
+    def canReach(self, nums: List[int], start: int) -> bool:
+        n = len(nums)
         
-        print(len(arr))
-        while q:
-            for _ in range(len(q)):
-                i = q.popleft()
-                if arr[i] == 0:
-                    return True
-                if -1< i - arr[i]<n and (i - arr[i]) not in seen:
-                    seen.add(i - arr[i])
-                    q.append(i - arr[i])
-                if -1< i + arr[i]<n and (i + arr[i]) not in seen:
-                    seen.add(i + arr[i])
-                    q.append(i + arr[i])
-        return False
+        @cache
+        def dfs(i):
+            if not 0 <= i < n:
+                return False
+            if nums[i] == 0:
+                return True
+            if nums[i] < 0:
+                return False
+            
+            nums[i] *= -1
+            
+            ans = dfs(i + nums[i]) or dfs(i - nums[i])
+            
+            nums[i] *= -1
+            
+            return ans
+        
+        return dfs(start)
