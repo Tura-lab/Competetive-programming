f6617ec149f5237c8eda30fd0b6b4a9726b45c47
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-12-03 18:16:15 +0300

    Time: 73 ms (73.11%), Space: 18.9 MB (17.04%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,4 +1,4 @@
 class Solution:
-    def frequencySort(self, s: str) -> str:      
+    def frequencySort(self, s: str) -> str:
         counter = Counter(s)
         return ''.join(sorted(list(sorted(s)), key = lambda l: counter[l], reverse = True))
