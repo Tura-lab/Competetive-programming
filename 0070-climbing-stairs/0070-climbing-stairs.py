6f7e9a02f179cdadbc48a2c2679bacbc4f906be5
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2022-12-12 10:45:15 +0300

    Time: 62 ms (20.89%), Space: 13.9 MB (58.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        third = 0
+        
+        for i in range(2, n):
+            third = first + second
+            first = second
+            second = third
+        
+        return second
+        
