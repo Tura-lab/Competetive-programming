75b1ed1be30ea8800417bf1d814b277caa8c855d
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-01-18 12:34:24 +0300

    Time: 288 ms (66.01%), Space: 14.9 MB (93.79%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -2,22 +2,17 @@ class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         '''
         00110
-        00
+        0011
         '''
+        zeros = s.count('0')
+        ones = 0
         
-        @cache
-        def dfs(i, turn):
-            if i == len(s):
-                return 0
-            
-            ans = float('inf')
-            if turn == 0:
-                ans = min(ans, (s[i] == '1') + min(dfs(i+1, turn), dfs(i+1, 1-turn)))
-                
-            if turn == 1:
-                ans = min(ans, (s[i] == '0') + dfs(i+1, turn))
-                
-            return ans
+        ans = zeros
         
-        return min(dfs(0, 0), dfs(0, 1))
+        for i in range(len(s)):
+            ones  += s[i] == '1'
+            zeros -= s[i] == '0'
             
+            ans = min(ans, ones + zeros)
+        
+        return ans
