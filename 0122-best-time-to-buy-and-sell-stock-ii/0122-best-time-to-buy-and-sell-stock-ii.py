7f2a8942cc58ff70dae58e48a454fd61034657aa
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-10-18 14:05:54 +0300

    Time: 66 ms (47.29%), Space: 17.7 MB (41.20%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
         n = len(prices)
         
-        @cache
-        def dfs(i, holding_stock):
-            if i == n:
-                return 0 if not holding_stock else -float('inf')
-            
-            if holding_stock:
-                return max(dfs(i + 1, holding_stock), dfs(i + 1, False) + prices[i])
-            
-            return max(dfs(i + 1, False), dfs(i + 1, True) - prices[i])
+        cur_hold, cur_not_hold = -float('inf'), 0
         
-        return dfs(0, False)
+        for price in prices:
+            prev_hold, prev_not_hold = cur_hold, cur_not_hold
+            
+            cur_hold = max(prev_not_hold - price, prev_hold)
+            cur_not_hold = max(prev_hold + price, prev_not_hold)
+            
+        return cur_not_hold
