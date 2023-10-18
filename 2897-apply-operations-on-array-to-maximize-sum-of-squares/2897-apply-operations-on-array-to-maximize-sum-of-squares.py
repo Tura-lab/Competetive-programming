ee151a17f32fc917a0b366ccfce4617bb30f4b8d
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-10-18 13:31:01 +0300

    Time: 4421 ms (5.00%), Space: 30 MB (98.09%) - LeetHub

---
+++ 2897-apply-operations-on-array-to-maximize-sum-of-squares/2897-apply-operations-on-array-to-maximize-sum-of-squares.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSum(self, nums: List[int], k: int) -> int:
+
+        mod = 10 ** 9 + 7
+        n = len(nums)
+        
+        counts = [0] * 33
+        nums.sort(reverse=True)
+        
+        for num in nums:
+            for i in range(33):
+                if 1 << i & num:
+                    counts[i] += 1
+                    
+        for j in range(len(nums)):
+            for i in range(33):
+                if counts[i]:
+                    nums[j] = nums[j] | 1 << i
+                    counts[i] -= 1
+                else:
+                    nums[j] = nums[j] & (~(1 << i))
+                    
+        nums.sort(reverse=True)
+        tot = 0
+        for i in range(k):
+            tot += nums[i] ** 2
+            tot %= mod
+            
+        return tot
