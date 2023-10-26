ed1e05e565843a81f213fcc501f1f6b32f3495d0
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-10-26 12:17:00 +0300

    Time: 564 ms (31.66%), Space: 16.4 MB (87.08%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -3,25 +3,15 @@ class Solution:
         arr.sort()
         arr_set = set(arr)
         MOD = 10 ** 9 + 7
-        
-        '''
-        2,4,5,10
-        
-        '''
-        
-        def dfs(root):
-            if root not in dp:
-                ans = 1
-                for num in arr:
-                    if root / num in arr_set:
-                        ans += dfs(num) * dfs(root / num)
-                        ans %= MOD
-                            
-                dp[root] = ans % MOD
-            return dp[root]
                                 
         dp = defaultdict()
         for num in arr:
-            dfs(num)
+            dp[num] = 1
+            
+        for num in arr:
+            for num2 in arr:
+                if num / num2 in arr_set:
+                    dp[num] += dp[num2] * dp[num / num2]
+                    dp[num] %= MOD
         
         return sum(dp.values()) % MOD
