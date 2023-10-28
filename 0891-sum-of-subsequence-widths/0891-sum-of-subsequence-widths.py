33b59ede9dbb0c4cff55b089ded49ba13fd4d24a
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-10-29 00:40:35 +0300

    Time: 1520 ms (17.84%), Space: 29.4 MB (51.59%) - LeetHub

--- 0891-sum-of-subsequence-widths/0891-sum-of-subsequence-widths.py
+++ 0891-sum-of-subsequence-widths/0891-sum-of-subsequence-widths.py
@@ -1,10 +1,5 @@
 class Solution:
     def sumSubseqWidths(self, nums: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        ans = 0
-        nums = sorted(nums)
-
-        for i, num in enumerate(nums):
-            ans += pow(2, i, MOD) * num - pow(2, len(nums) - i - 1, MOD) * num
             
-        return ans % MOD
+        return sum(pow(2, i, MOD) * num - pow(2, len(nums) - i - 1, MOD) * num for i, num in enumerate(sorted(nums))) % MOD
