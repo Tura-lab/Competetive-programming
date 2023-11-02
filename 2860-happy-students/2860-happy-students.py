05151348bbb9ca403cffc15cfd335a48de9cadf9
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-11-02 20:37:59 +0300

    Time: 575 ms (7.07%), Space: 28.7 MB (28.73%) - LeetHub

--- 2860-happy-students/2860-happy-students.py
+++ 2860-happy-students/2860-happy-students.py
@@ -5,16 +5,15 @@ class Solution:
         
         """
         nums.sort()
-        next_greater = []
+        next_greater = [0] * len(nums)
         past = float('inf')
         for i in range(len(nums) - 1, -1, -1):
-            next_greater.append(past)
+            next_greater[i] = past
             if i > 0 and nums[i - 1] != nums[i]:
                 past = nums[i]
 
-        next_greater = next_greater[::-1]
-        ans = 0 not in nums
         
+        ans = 0 not in nums
         for i, num in enumerate(nums):
             if i < len(nums) - 1 and nums[i] == nums[i + 1]:
                 continue
