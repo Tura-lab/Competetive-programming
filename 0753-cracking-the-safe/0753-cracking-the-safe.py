97d41a6f483ab7b8b2714b402dd59d8859a0a82d
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-11-04 00:25:54 +0300

    Time: 50 ms (69.43%), Space: 21.6 MB (51.30%) - LeetHub

--- 0753-cracking-the-safe/0753-cracking-the-safe.py
+++ 0753-cracking-the-safe/0753-cracking-the-safe.py
@@ -1,20 +1,39 @@
 class Solution:
     def crackSafe(self, n: int, k: int) -> str:
-        cur = "0" * (n - 1)
+        
+        def search(size):
+            found = []
+            temp = []
+            def dfs():
+                nonlocal temp
+                if len(temp) == size:
+                    found.append("".join(temp))
+                    return
+                for i in range(k):
+                    temp.append(str(i))
+                    dfs()
+                    temp.pop()
+        
+            dfs()
+            return found
+
+        graph = defaultdict(list)
+        
+        # generate all n sized passwords
+        perms = set(search(n))
         
         ans = []
-        visited = set()
-        
         def dfs(node):
             for i in range(k):
-                # so i is the edge that connects the two nodes
-                cur_node = node + str(i)
-                
-                if cur_node not in visited:
-                    visited.add(cur_node)
-                    dfs(cur_node[1:])
+                new = node + str(i)
+                if new in perms:
+                    perms.remove(new)
+                    dfs(new[1:])
                     ans.append(str(i))
-                    
+                
+        cur = "0" * (n - 1)
         dfs(cur)
-                    
         return "".join(ans) + cur
+    
+    
+    
