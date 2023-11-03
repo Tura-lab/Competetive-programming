65d4e13b8904509d94fe084955f61b5d553f97a1
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-11-03 17:08:54 +0300

    Time: 2216 ms (67.83%), Space: 84.3 MB (89.56%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        # count indeg and outdeg to check if graph is eulerian
         graph = defaultdict(list)
         outdeg = defaultdict(int)
         indeg = defaultdict(int)
@@ -8,10 +9,8 @@ class Solution:
             indeg[b] += 1
             outdeg[a] += 1
             graph[a].append(b)
-        
-        
+
         ans = []
-        
         start = pairs[0][0]
         for a in outdeg:
             if outdeg[a] == 1 + indeg[a]:
@@ -23,15 +22,13 @@ class Solution:
         stack.append(start)
         while stack:
             cur_node = stack[-1]
-            if outdeg[cur_node] > 0:
+            if graph[cur_node]:
                 nxt_node = graph[cur_node].pop()
                 stack.append(nxt_node)
-                outdeg[cur_node] -= 1
                 cur_node = nxt_node
             else:
                 ans.append(cur_node)
                 stack.pop()
                 
         ans.reverse()
-        # print(ans)
         return [[ans[i - 1], ans[i]] for i in range(1, len(ans))]
