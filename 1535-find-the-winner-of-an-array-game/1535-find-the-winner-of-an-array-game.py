2a9d5697438d3469de82a4a68133d31a3ecfaa0c
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-11-05 10:50:53 +0300

    Time: 600 ms (22.83%), Space: 26.6 MB (100.00%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,22 +1,21 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        next_larger = [n for _ in range(len(arr))]
-        
-        stack = []
-        
-        for i, num in enumerate(arr):
-            while stack and num > stack[-1][0]:
-                val, idx = stack.pop()
-                next_larger[idx] = i
-            stack.append((num, i))
-        mx = 0    
-        for i in range(len(arr)):
-            wins = next_larger[i] - i - 1 + (i > 0 and arr[i-1] < arr[i])
+        for i in range(1, n - 1):
+            arr[i] = max(arr[i], arr[i - 1])
             
-            if wins >= k:
-                return arr[i]
+        arr[0] = 0
+        mx = 0
+        past, count = 0, 0     
+        for num in arr:
+            if num == past:
+                count += 1
+            else:
+                past = num
+                count = 1
             
-            mx = max(mx, arr[i])
+            if count >= k and num != 0:
+                return num
+            mx = max(mx, num)
             
         return mx
