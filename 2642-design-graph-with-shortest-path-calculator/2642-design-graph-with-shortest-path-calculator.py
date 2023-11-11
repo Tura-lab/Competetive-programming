45cfdc1c7668c24b81f652264e0fcfaee00d4d0c
Tiruzer Tsedeke <55505646+Tura-lab@users.noreply.github.com>
2023-11-11 17:29:08 +0300

    Time: 835 ms (44.12%), Space: 19.5 MB (60.59%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
@@ -1,37 +1,32 @@
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
-        self.distances = [[float('inf')] * n for _ in range(n)]
-        self.n = n
+        self.graph = defaultdict(list)
         
         for a, b, w in edges:
-            self.distances[a][b] = w
-        
-        for i in range(n):
-            self.distances[i][i] = 0
-            
-        self.floyd_warshal()
-        
-    def floyd_warshal(self):
-        for k in range(self.n):
-            for i in range(self.n):
-                for j in range(self.n):
-                    self.distances[i][j] = min(self.distances[i][j], self.distances[i][k] + self.distances[k][j])
+            self.graph[a].append((b, w))
 
     def addEdge(self, edge: List[int]) -> None:
         a, b, w = edge
-        if w >= self.distances[a][b]:
-            return
+        self.graph[a].append((b, w))
         
-        self.distances[a][b] = w
-        for i in range(self.n):
-            for j in range(self.n):
-                self.distances[i][j] = min(self.distances[i][j], self.distances[i][a] + self.distances[a][b] + self.distances[b][j])
-
     def shortestPath(self, node1: int, node2: int) -> int:
-        ans = self.distances[node1][node2]
-        return ans if ans < float('inf') else -1
-
+        visited = set()
+        q = [(0, node1)]
+        while q:
+            cost, node = heappop(q)
+            
+            if node == node2:
+                return cost
+            
+            visited.add(node)
+            for v, w in self.graph[node]:
+                if v not in visited:
+                    heappush(q, (cost + w, v))
+            
+        return -1
+            
+        
 # Your Graph object will be instantiated and called as such:
 # obj = Graph(n, edges)
 # obj.addEdge(edge)
